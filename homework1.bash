33c1e42b20f00727dd1d1710e58ee4c7da8fbfdd
Andrew <excisionhd1@gmail.com>
2018-09-25 21:38:22 -0700

    add first homework

---
+++ homework1.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE_DIR=usr/share/dict/words
+WORD_COUNT=$(cat $FILE_DIR | wc -l)
+RANDOM_NUM=$(($RANDOM % $WORD_COUNT+1))
+
+cat usr/share/dict/words | grep -v "'" | head -n $RANDOM_NUM | tail -n 1
+ 
+
+
+
